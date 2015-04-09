0f64348c1b5c7da660199012410583d04cbcb23a
lolisa <lolisa@marisa.moe>
2015-04-10 00:04:02 +0800

    Add named_parameter.

--- can_call.hpp
+++ can_call.hpp
@@ -1,10 +1,10 @@
 #ifndef CAN_CALL_HPP
 #define CAN_CALL_HPP
 #include <boost/mpl/bool.hpp>
+#include <utility>
 namespace common
 {
     template< typename T > struct PrintType;
-    template< typename  T > T FakeType( );
     template< typename T, typename ... R >
     struct can_call
     {
@@ -12,7 +12,7 @@ namespace common
         static constexpr boost::mpl::true_ SFINAE(
                 t,
                 std::remove_reference_t
-                < decltype( FakeType< t >( )( FakeType< R >( )... ) ) > * = nullptr )
+                < decltype( std::declval< t >( )( std::declval< R >( )... ) ) > * = nullptr )
         { return boost::mpl::true_( ); }
         static constexpr boost::mpl::false_ SFINAE( ... ) { return boost::mpl::false_( ); }
         static constexpr bool value = decltype( SFINAE( std::declval< T >( ) ) )::value;
--- cpp_common.pro
+++ cpp_common.pro
@@ -12,5 +12,6 @@ HEADERS += \
     iterator.hpp \
     restriction.hpp \
     value_less.hpp \
-    scope.hpp
+    scope.hpp \
+    named_parameter.hpp
 QMKAE_CXXFLAGS += -std=c++1z
